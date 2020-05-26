b6714b73e1eda54c29d1d1fedf97e7bc2da16448
Begana Choi <begana001.jpg@gmail.com>
2020-05-26 18:39:19 +0200

    .

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ const projectRoutes = require('./routes/project');
 app.use(mainRoutes);
 app.use('/project', projectRoutes);
 
+app.use( ( req,res,next ) => {
+    const err = new Error('Oh No! Page Not Found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use( ( req,res,next ) => {
+    const err = new Error('Something went wrong!');
+    err.status = 500;
+    next(err);
+});
+
+app.use( ( err,req,res,next ) => {
+    const error = err;
+    console.log(error.message)
+    res.render('error', { error });
+});
 
 app.listen('3000', () => {
     console.log('this app is running on localhost:3000');
